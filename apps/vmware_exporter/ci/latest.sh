8b2c5e89646b5400e6d657f7d39c866bf14e1d4d
Thomas Deutsch <thomas@tuxpeople.org>
2023-05-14 17:25:51 +0200

    fixes

--- apps/vmware_exporter/ci/latest.sh
+++ apps/vmware_exporter/ci/latest.sh
@@ -2,7 +2,4 @@
 version=$(curl -sX GET https://api.github.com/repos/pryorda/vmware_exporter/releases/latest | jq --raw-output '. | .tag_name' 2>/dev/null)
 version="${version#*v}"
 version="${version#*release-}"
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-git clone https://github.com/pryorda/vmware_exporter.git . >/dev/null 2>&1
-git checkout tags/v${version} >/dev/null 2>&1
 printf "%s" "${version}"
