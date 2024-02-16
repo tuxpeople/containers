da504ed9966686a447a4d1051ef916fe9a79c6cb
Thomas Deutsch <thomas@tuxpeople.org>
2024-02-16 15:17:38 +0100

    fix git versions

--- apps/alertmanager-discord/ci/latest.sh
+++ apps/alertmanager-discord/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET https://api.github.com/repos/benjojo/alertmanager-discord/commits/master | jq -r .sha | cut -c1-7 2>/dev/null)
+version=git-$(curl -sX GET https://api.github.com/repos/benjojo/alertmanager-discord/commits/master | jq -r .sha | cut -c1-7 2>/dev/null)
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
