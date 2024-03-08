f1a7e8741d39f8522179dbf70f6de452b55ca44d
Thomas Deutsch <thomas@tuxpeople.org>
2024-03-08 10:06:37 +0100

    adjust version

--- apps/ubuntu-weekly/ci/latest.sh
+++ apps/ubuntu-weekly/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version="$(date --date='last friday' +0.%Y.%m%d)"
+version="24.04.$(date --date='last friday' +%Y%m%d)"
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
