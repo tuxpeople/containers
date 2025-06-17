50faa11017b581070e38b0d34a6c2e98d767ad57
Thomas Deutsch <thomas@tuxpeople.org>
2025-06-17 17:31:06 +0200

    fixes

--- apps/busycurl/ci/latest.sh
+++ apps/busycurl/ci/latest.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-version="0.0.$(date --date='last friday' +%Y%m%d)"
+
+# renovate: datasource=docker depName=library/busybox
+BUSYBOX_VERSION=1.37.0-musl
+
+# renovate: datasource=github-releases depName=curl/curl
+CURL_VERSION=8.11.0
+
+version="${BUSYBOX_VERSION}+${CURL_VERSION}"
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
