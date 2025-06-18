6c215b032a6d8c069064d23bbc9a1b26967a0fb3
Thomas Deutsch <thomas@tuxpeople.org>
2025-06-18 08:17:44 +0200

    remove renovate for curl

--- apps/busycurl/ci/latest.sh
+++ apps/busycurl/ci/latest.sh
@@ -3,8 +3,8 @@
 # renovate: datasource=docker depName=library/busybox
 BUSYBOX_VERSION=1.37.0-musl
 
-# renovate: datasource=github-releases depName=curl/curl
-CURL_VERSION=8.11.0
+# r e n o v a t e: datasource=github-releases depName=curl/curl
+CURL_VERSION=$(curl -sX GET https://api.github.com/repos/curl/curl/releases/latest | jq -r .name)
 
 version="${BUSYBOX_VERSION}+${CURL_VERSION}"
 version="${version#*v}"
